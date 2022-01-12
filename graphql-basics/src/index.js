232eadcb8a98aca3a190564ecc0a7e41cdd02ff4
rejan buzzracharyeah <rejandev@gmail.com>
2022-01-12 15:32:45 +0545

    init commit

---
+++ graphql-basics/src/index.js
@@ -0,0 +1,5 @@
+import name, { add, subtract, test } from './math';
+
+console.log(add(1, 2));
+console.log(subtract(3, 4));
+console.log(name);
---
+++ graphql-basics/src/math.js
@@ -0,0 +1,9 @@
+const name = 'reajn';
+export const test = 'test';
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+export { add, subtract, name as default };
+
+console.log('say hi');
