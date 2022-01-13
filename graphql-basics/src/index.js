17ac4b6bd5cca702fedabbaf8ef4a1a1c5170f55
rejan buzzracharyeah <rejandev@gmail.com>
2022-01-13 12:06:24 +0545

    operation args test

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,8 +2,11 @@ import { createServer } from 'graphql-yoga';
 
 const typeDefs = `
 type Query {
+  users: [User!]!
   me: User
   greeting(name: String, role: String!): String! 
+  rewards: [Float!]!
+  rewardsTotal(rewards: [Float]):Float!
   
 }
 
@@ -26,12 +29,23 @@ const resolvers = {
         married: false,
       };
     },
+    rewards() {
+      return [1, 2, 3];
+    },
     greeting(parent, args, ctx, info) {
       if (args) {
+        // console.log({ parent, args, ctx, info });
         return `hello ${args.name}. you\'re the greatest ${args.role} of all time`;
       }
 
       return "Hi there. hope you're well";
+    },
+    rewardsTotal(parent, args, ctx, info) {
+      if (args.rewards) {
+        const rewardsTotal = args.rewards.reduce((acc, current) => acc + current, 0);
+        return rewardsTotal;
+      }
+      return 0;
     },
   },
 };
